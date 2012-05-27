3df3c7e17715959dec895738898204a4350c20f0
Brian Whitmer <brian@instructure.com>
2012-05-26 23:27:41 -0600

    allow badge urls from other domains

--- canvabadges.rb
+++ canvabadges.rb
@@ -154,6 +154,7 @@ end
 def badge_data(params)
   badge = Badge.first(:course_id => params[:course_id], :user_id => params[:user_id], :nonce => params[:code])
   headers 'Content-Type' => 'application/json'
+  badge.badge_url = "https://#{request.host_with_port}" + badge.badge_url if badge.badge_url.match(/^\//)
   if badge
     return {
       :recipient => badge.recipient,
@@ -162,7 +163,7 @@ def badge_data(params)
       :badge => {
         :version => "0.5.0",
         :name => badge.name,
-        :image => "https://#{request.host_with_port}#{badge.badge_url}",
+        :image => badge.badge_url,
         :description => badge.description,
         :criteria => "/badges/#{badge.id}/criteria",
         :issuer => {
