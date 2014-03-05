c238b72d96e5e92fc40e90c049a76e455de03eb0
Brian Whitmer <brian@instructure.com>
2014-03-05 16:23:25 -0700

    use login_id as email fallback

--- lib/badge_configuration.rb
+++ lib/badge_configuration.rb
@@ -107,11 +107,15 @@ module Sinatra
         if @badge_config && @badge_config.configured? && (@badge_placement_config.configured? || @badge_placement_config.award_only?)
           json = api_call("/api/v1/courses/#{@course_id}/users?enrollment_type=student&per_page=50&include[]=email&user_id=#{params['user_id']}", @user_config)
           student = json.detect{|e| e['id'] == params['user_id'].to_i }
+          email = student['email'] 
+          if !email && student['login_id'] && student['login_id'].match(/@/)
+            email = student['login_id']
+          end
           if student
-            if !student['email']
+            if !email
               return error("That user doesn't have an email in Canvas, and so can't be awarded badges. Please notify the student that they need to set up an email address and then try again.")
             end
-            badge = Badge.manually_award(params, @badge_placement_config, student['name'], student['email'])
+            badge = Badge.manually_award(params, @badge_placement_config, student['name'], email)
             
             redirect to("/badges/check/#{@badge_placement_config_id}/#{@user_id}")
           else
